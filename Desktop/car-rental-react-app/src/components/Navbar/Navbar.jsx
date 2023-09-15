2fcf763d78ada2603599b7141da19ee319414fea
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-09-15 15:28:58 +0100

    added a function for the nav-bar

--- Desktop/car-rental-react-app/src/components/Navbar/Navbar.jsx
+++ Desktop/car-rental-react-app/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.css'
-import {AiOutlineMenu, AiOutlineclose, AiOutlineSearch, AiOutlineUser} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineClose, AiOutlineSearch, AiOutlineUser} from 'react-icons/ai'
 import logo from '../../images/logo.png'
 
 const Navbar = () => {
+    const[nav, setNav] = useState(false)
   return (
     <header className={styles.navbar}>
         <img src={logo} alt='/' />
         <nav>
-            <ul className={styles.menu}>
+            <ul className={nav ? [styles.menu, styles.active].join(" ") : [styles.menu]}>
                 <li><a href="/">Learn More</a></li>
                 <li><a href="/">Log In</a></li>
                 <li><a href="/">Sign Up</a></li>
@@ -16,8 +17,10 @@ const Navbar = () => {
                 <li><AiOutlineUser size={25} style={{marginTop: '6px'}}/></li>
             </ul>
         </nav>
-        <div className={styles.mobile_btn}>
-            <AiOutlineMenu size={25} />
+        <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
+            {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+            
+            
         </div>
     </header>
   )
